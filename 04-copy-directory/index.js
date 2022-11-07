60878bfaa98599f392cf8b644762b233b8841ca0
terrign <vajipi@gmail.com>
2022-11-08 02:34:41 +0300

    feat: 04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy')
+       ,(err) => {if(err) console.error(err)})
+
+fs.readdir(path.join(__dirname,'files')
+          ,{withFileTypes: true}
+          ,(err,files) => {
+    if (err) console.error(err)
+    else files.forEach(file => {
+        fs.copyFile(path.join(__dirname,'files', file.name)
+                   ,path.join(__dirname,'files-copy', file.name)
+                   ,(err) => {if(err) console.error(err)})
+    })
+})
