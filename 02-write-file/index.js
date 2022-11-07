aba7f6828642652f59ee524c6dd4e8d2852c228a
terrign <vajipi@gmail.com>
2022-11-07 22:01:06 +0300

    feat: 02-write-file and ctrl+c listener

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,19 +3,20 @@ const path = require('path');
 const { stdin, stdout } = process;
 const output = fs.createWriteStream(path.join(__dirname,'text.txt'));
 
-
 stdout.write('Please enter text or type "exit" if you want to exit\n')
 
 output.on('data', chunk => output.write(chunk))
 
+process.on('SIGINT', () => process.exit())
+
+process.on('exit', () => stdout.write('Good bye'))
+
 stdin.on('data', data => {
     const str = data.toString()
-    if (str.trim() == 'exit') {
-        stdout.write('Good bye')
-        process.exit()
-    }
+    if (str.trim() == 'exit') process.exit()
     else output.write(str)  
 })
+
 
 
 
