386ee0f46cc7303dd9f0271ac6b03cd4263770c8
terrign <vajipi@gmail.com>
2022-11-08 01:42:54 +0300

    03-file-in-folder done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout} = process;
+
+
+
+fs.readdir(path.join(__dirname,'secret-folder')
+          ,{withFileTypes: true}
+          ,(err,files) => {
+    if (err) console.error(err)
+    else files.forEach(file => {
+        fs.stat(path.join(__dirname,'secret-folder',file.name)
+               ,(err,stats) => {
+            if (err) console.error(err)
+            else {
+                if(file.isDirectory() === false) stdout.write(file.name.replace('.', ' - ') + ' - ' + stats.size/8 + ' kb\n')
+            }
+        })
+    })
+})
