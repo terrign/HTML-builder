de9f8030889b4a3197d1c60643d92d0254e67bed
terrign <vajipi@gmail.com>
2022-11-07 19:23:31 +0300

    feat: 01-read-file done

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+const readableStream = fs.createReadStream(path.join(__dirname,'text.txt'));
+
+readableStream.on('data', data => stdout.write(data))
+    
+
+
