9f1b5c2efb862f069221fec28d2140c6d806c2ca
terrign <vajipi@gmail.com>
2022-11-08 03:31:22 +0300

    feat: 05-merge-styles done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname,'styles')
+          ,{withFileTypes: true}
+          ,(err,files) => {
+    if (err) console.error(err)
+    else files.forEach(file => {
+        if (err) console.error(err)
+        else {
+            if(file.isDirectory() === false && path.extname(file.name) == '.css') {
+                const readableStream = fs.createReadStream(path.join(__dirname,'styles',file.name));
+                readableStream.on('data', (data) => {
+                    fs.appendFile(path.join(__dirname, 'project-dist','bundle.css')
+                                            ,data
+                                            ,err => {if (err) console.error(err)})
+                });
+            }
+        }
+    })
+})
+
+
+
+
+
+
