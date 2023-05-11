80c7dec3d69adac746923a6379910d0c6c09029b
terrign <vajipi@gmail.com>
2023-05-11 11:29:43 +0300

    fix: task 4 remove sync method

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,11 @@
-const fs = require('fs');
 const path = require('path');
+const fp = require('fs/promises');
 
 
-fs.rmSync(path.join(__dirname, 'files-copy'), { recursive: true, force: true });
-
-fs.mkdir(path.join(__dirname, 'files-copy')
-  , (err) => {if(err) console.error(err);});
-
-fs.readdir(path.join(__dirname,'files')
-  , {withFileTypes: true}
-  , (err, files) => {
-    if (err) console.error(err);
-    else files.forEach(file => {
-      fs.copyFile(path.join(__dirname,'files', file.name)
-        ,path.join(__dirname,'files-copy', file.name)
-        ,(err) => {if(err) console.error(err);});
-    });
+(async function () {
+  await fp.mkdir(path.join(__dirname, 'files-copy'), {recursive:true});
+  const files = await fp.readdir(path.join(__dirname,'files'), {withFileTypes: true});
+  files.forEach(async function (file) {
+    await fp.copyFile(path.join(__dirname,'files', file.name), path.join(__dirname,'files-copy', file.name));
   });
+})();
